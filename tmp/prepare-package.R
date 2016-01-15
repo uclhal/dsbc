abf4afffdc9fc7543b6a947cf97170dc9fd45c72
Steve Harris (docsteveharris) <git@steveharris.me>
2016-01-15 10:56:45 +0000

    removed tmp subdir from index

--- tmp/prepare-package.R
+++
@@ -1,9 +0,0 @@
-# Code to create documentation and install
-document()
-setwd("/Users/steve/Sites/datascibc/dsbc")
-install("../dsbc")
-library(dsbc)
-# Testing lookfor function
-test_data <- data.frame(id=c(1:10), x=seq(1, 100, 10))
-lookfor("x", data=test_data)
-?lookfor
